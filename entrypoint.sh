e970a525775193009fbf690915bd39f104abfe40
Seiya Yazaki <saiya.v6@gmail.com>
2019-03-04 13:35:41 +0900

    Improve file check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z "$(ls -A ${DOKUWIKI_ROOT})" ]; then
+if [ ! -f "${DOKUWIKI_ROOT}/index.php" ]; then
   echo "Dokuwiki root directory (${DOKUWIKI_ROOT}) is empty, try to restore from backup"
   restore_dokuwiki_from_backup || echo "Backup restore failed, ${DOKUWIKI_ROOT} is still empty"
 fi
